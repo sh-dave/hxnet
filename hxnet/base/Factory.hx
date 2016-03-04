652ff0602aecf13ce7433502cf9592367f6ff2a7
David Klein <dave@smokinheads.de>
2016-03-04 10:39:48 +0100

    Factory forwards arguments to Protocol

--- hxnet/base/Factory.hx
+++ hxnet/base/Factory.hx
@@ -4,15 +4,17 @@ import hxnet.interfaces.Protocol;
 
 class Factory implements hxnet.interfaces.Factory
 {
-	public function new(protocol:Class<Protocol>)
+	public function new(protocol:Class<Protocol>, ?args:Array<Dynamic>)
 	{
 		this.protocol = protocol;
+		this.args = args;
 	}
 
 	public function buildProtocol():Protocol
 	{
-		return Type.createInstance(protocol, []);
+		return Type.createInstance(protocol, args == null ? [] : args);
 	}
 
 	private var protocol:Class<Protocol>;
+	private var args:Array<Dynamic>;
 }
