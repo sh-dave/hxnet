a8cb3a837d98b7991a742ed07dfef53ebc32aeed
David Klein <dave@smokinheads.de>
2016-03-09 08:53:25 +0100

    removed connected callback from Client.connect() and put it into tcp.Client

--- hxnet/interfaces/Client.hx
+++ hxnet/interfaces/Client.hx
@@ -2,7 +2,7 @@ package hxnet.interfaces;
 
 interface Client
 {
-	public function connect(?hostname:String, ?port:Null<Int>, ?callback:Bool->String->Void):Void;
+	public function connect(?hostname:String, ?port:Null<Int>):Void;
 	public function update(timeout:Float=1):Void;
 	public function close():Void;
 }
--- hxnet/tcp/Client.hx
+++ hxnet/tcp/Client.hx
@@ -18,46 +18,24 @@ class Client implements hxnet.interfaces.Client
 	public var protocol(default, set):Protocol;
 	public var blocking(default, set):Bool = true;
 	public var connected(get, never):Bool;
-	public var disconnected(default, default):String->Void;
+	public var disconnectedHandler(default, default):String->Void;
+	public var connectedHandler(default, default):Bool->String->Void;
 
 	public function new()
 	{
 		buffer = Bytes.alloc(8192);
 	}
 
-	public function connect(?hostname:String, port:Null<Int> = 12800, callback:Bool->String->Void = null)
+	public function connect(?hostname:String, port:Null<Int> = 12800)
 	{
 		try
 		{
 			client = new Socket();
 #if flash
+			client.addEventListener(Event.CONNECT, client_connectHandler);
+			client.addEventListener(flash.events.IOErrorEvent.IO_ERROR, client_ioErrorHandler);
+			client.addEventListener(flash.events.SecurityErrorEvent.SECURITY_ERROR, client_securityErrorHandler);
 			client.connect(hostname, port);
-
-			client.addEventListener(Event.CONNECT, function( _ ) {
-				if (protocol != null) {
-					protocol.onConnect(new Connection(client));
-				}
-
-				if (callback != null) {
-					callback(true, null);
-				}
-
-				flashConnectedFlag = true;
-			});
-
-			client.addEventListener(flash.events.IOErrorEvent.IO_ERROR, function( event : flash.events.IOErrorEvent ) {
-				flashConnectedFlag = false;
-
-				if (callback != null) {
-					callback(false, event.text);
-				}
-			});
-
-			client.addEventListener(flash.events.SecurityErrorEvent.SECURITY_ERROR, function( event : flash.events.SecurityErrorEvent ) {
-				if (callback != null) {
-					callback(false, event.text);
-				}
-			});
 #else
 			if (hostname == null) hostname = Host.localhost();
 			client.connect(new Host(hostname), port);
@@ -72,9 +50,9 @@ class Client implements hxnet.interfaces.Client
 				protocol.onConnect(new Connection(client));
 			}
 
-			if (callback != null) {
+			if (connectedHandler != null) {
 				// TODO (DK) can we actually be sure we're connected?
-				callback(true, null);
+				connectedHandler(true, null);
 			}
 #end
 		}
@@ -83,11 +61,41 @@ class Client implements hxnet.interfaces.Client
 			trace(e);
 			client = null;
 
-			if (callback != null) {
-				callback(false, Std.string(e));
+			if (connectedHandler != null) {
+				connectedHandler(false, Std.string(e));
 			}
 		}
 	}
+
+#if flash
+	function client_connectHandler( _ ) {
+		if (protocol != null) {
+			protocol.onConnect(new Connection(client));
+		}
+
+		if (connectedHandler != null) {
+			connectedHandler(true, null);
+		}
+
+		flashConnectedFlag = true;
+	}
+
+	function client_ioErrorHandler( event : flash.events.IOErrorEvent ) {
+		flashConnectedFlag = false;
+
+		if (connectedHandler != null) {
+			connectedHandler(false, event.text);
+		}
+	}
+
+	function client_securityErrorHandler( event : flash.events.SecurityErrorEvent ) {
+		flashConnectedFlag = false;
+
+		if (connectedHandler != null) {
+			connectedHandler(false, event.text);
+		}
+	}
+#end
 
 	public function update(timeout:Float=0)
 	{
@@ -130,10 +138,10 @@ class Client implements hxnet.interfaces.Client
 				}
 
 #if flash
-				//flashConnectedFlag = false;
+				flashConnectedFlag = false;
 #end
-				if (disconnected != null) {
-					disconnected(Std.string(e));
+				if (disconnectedHandler != null) {
+					disconnectedHandler(Std.string(e));
 				}
 			//}
 		}
@@ -183,6 +191,10 @@ class Client implements hxnet.interfaces.Client
 	{
 		if (client != null) {
 #if flash
+			client.removeEventListener(Event.CONNECT, client_connectHandler);
+			client.removeEventListener(flash.events.IOErrorEvent.IO_ERROR, client_ioErrorHandler);
+			client.removeEventListener(flash.events.SecurityErrorEvent.SECURITY_ERROR, client_securityErrorHandler);
+
 			if (client.connected)
 #end
 				client.close();
@@ -195,8 +207,12 @@ class Client implements hxnet.interfaces.Client
 			protocol = null;
 		}
 
-		if (disconnected != null) {
-			disconnected('close requested');
+#if flash
+		flashConnectedFlag = false;
+#end
+
+		if (disconnectedHandler != null) {
+			disconnectedHandler('close requested');
 		}
 	}
 
